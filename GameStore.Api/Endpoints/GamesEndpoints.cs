faa8c49dbc4aeb6fc7bf1e1c19c528da73a58db4
Elsie <ojemba@Ojembas-MacBook-Pro.local>
2025-05-26 10:25:13 +0200

    asap .net gamestore project

---
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -0,0 +1,84 @@
+using GameStore.Api.Dtos;
+
+namespace GameStore.Api.Endpoints;
+
+public static class GamesEndpoints
+{
+    const string GetGameRouteName = "GetGame";
+
+    private static readonly List<GameDto> games = [
+    new GameDto(
+        1, 
+        "The Last of Us Part II", "Action-adventure", 
+        59.99m, 
+        new DateOnly(2020, 6, 19)),
+    new GameDto(
+        2, 
+        "Ghost of Tsushima", 
+        "Action-adventure", 
+        59.99m, 
+        new DateOnly(2020, 7, 17)),
+    new GameDto(
+        3, 
+        "Cyberpunk 2077", 
+        "Action role-playing", 
+        59.99m, 
+        new DateOnly(2020, 12, 10))
+    ];
+
+    public static RouteGroupBuilder MapGamesEndpoints(this WebApplication app){
+        var group = app.MapGroup("games");
+
+        group.MapGet("/", () => games);
+
+        group.MapGet("/{id}", (int id) =>
+        {
+            GameDto? game = games.Find(game => game.Id == id);
+
+            return game is null ? Results.NotFound() : Results.Ok(game);
+        }).WithName(GetGameRouteName);
+
+        group.MapPost("/", (CreateGameDto newGame) =>
+        {
+            GameDto game = new(
+                games.Count + 1,
+                newGame.Name,
+                newGame.Genre,
+                newGame.Price,
+                newGame.ReleaseDate
+            );
+
+            games.Add(game);
+
+            return Results.CreatedAtRoute(GetGameRouteName, new { id = game.Id }, game);
+        });
+
+        group.MapPut("/{id}", (int id, UpdateGameDto updatedGame) =>
+        {
+            var index = games.FindIndex(game => game.Id == id);
+
+            if (index == -1)
+            {
+                return Results.NotFound();
+            }
+
+            games[index] = new GameDto(
+                id,
+                updatedGame.Name,
+                updatedGame.Genre,
+                updatedGame.Price,
+                updatedGame.ReleaseDate
+            );
+
+            return Results.NoContent();
+        });
+
+        group.MapDelete("/{id}", (int id) =>
+        {
+            games.RemoveAll(game => game.Id == id);
+
+            return Results.NoContent();
+        });
+     return group;
+    }
+}
